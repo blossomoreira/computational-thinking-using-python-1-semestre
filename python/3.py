676a9b97f450f57fd4a63a8f3c67ac06cf28d69e
Diogo Moreira <104456651+diogomoreirax@users.noreply.github.com>
2023-03-01 20:55:06 -0300

    oi

---
+++ python/3.py
@@ -0,0 +1,13 @@
+nota1 = float(input("digite a primeira nota: "))
+nota2 = float(input("digite a segunda nota 2"))
+nota3 = float(input("digite a segunda nota 3"))
+
+media = (nota + nota2 + nota3) / 3
+
+print(f"a media das notas eh: {media: .2f}")
+
+#f quando qer formatar o print
+#. = casa decimal
+#2 = duas casas
+#f = float
+
