dfa3d54ae125251bf57853056f3d595774ec45be
diogomoreirax <rm99478@fiap.com.br>
2023-04-02 15:36:33 -0300

    socorro

---
+++ atividade 2/1soma_sub_mult.py
@@ -0,0 +1,19 @@
+# Considerando 2 números inteiros, faça a soma, subtração e multiplicação.
+# Por fim, mostre os resultados.
+
+num1 = int (input("Digite um número inteiro aqui"))
+num2 = int (input("Digite mais um número inteiro aqui"))
+
+soma = num1 + num2
+
+print (("O resultado da sua soma é "),(soma))
+
+sub = num1 - num2
+
+print(("O resultado da sua subtração é: "),(sub))
+
+
+multi = num1 * num2
+
+print(("O resultado da sua multiplicação é: "),(multi))
+
---
+++ atividade 2/5salario.py
@@ -0,0 +1,9 @@
+# Tendo como base o salário de um funcionário, faça o acréscimo de 20%
+# sobre seu valor e exiba-o. DICA: para fazer o acréscimo, multiplique o valor
+# do salário por 1,20.
+
+salario = float (input("Digite o salário: "))
+
+res = salario * 1.20
+
+print(("O salário com acréscimo de 20% é: "),(res))
